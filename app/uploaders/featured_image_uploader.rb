d8c4a5e2b8253e2d01b9541d620a780ead27e2fb
Rowan Hogan <rowan.hogan@gmail.com>
2014-12-07 14:30:11 +1000

    Shows article images

--- app/uploaders/featured_image_uploader.rb
+++ app/uploaders/featured_image_uploader.rb
@@ -7,6 +7,12 @@ class FeaturedImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  process :resize_to_fill => [2000, 1500]
+
+  version :medium do
+    process :resize_to_fill => [300, 200]
+  end
+
   version :thumb do
     process :resize_to_fill => [50, 50]
   end
